c455525dc0657eed630e4e5d23a8834bf0b64ed8
jenkim127 <jennifer_kim@Jennifers-MacBook-Pro.local>
2014-07-15 18:57:13 -0400

    Basic Calculator

---
+++ Basic Calculator/Basic Calculator.py
@@ -0,0 +1,18 @@
+Python 3.4.1 (v3.4.1:c0e311e010fc, May 18 2014, 00:54:21) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
+Visit http://www.python.org/download/mac/tcltk/ for current information.
+print("basic calculator")
+basic calculator
+>>> x=127
+>>> y=92
+>>> print(x+y)
+219
+>>> print(x-y)
+35
+>>> print(x*y)
+11684
+>>> print(x/y)
+1.3804347826086956
+>>> 
